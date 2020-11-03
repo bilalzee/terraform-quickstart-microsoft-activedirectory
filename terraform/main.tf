578dc4dfdd55ec8703a37bba799f518943452a4f
bilalzee <bilalzee@gmail.com>
2020-11-03 18:33:33 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,10 +3,11 @@ module "ad-master-1" {
 
   ### Important - we need to work on hiding these variables.  they should not exist in the git repo.  
   ### You will need to create your own private-variables.tf file in this folder to define these variables uniquely to your deployment.
-
+  AvailabilityZones  = "${var.AvailabilityZones}"
   KeyPairName         = "${var.KeyPairName}"
   RDGWCIDR            = "${var.RDGWCIDR}"
   // RestoreModePassword = "${var.RestoreModePassword}"
   DomainAdminUser     = "${var.DomainAdminUser}"
   DomainAdminPassword = "${var.DomainAdminPassword}"
+  DomainDNSName       = "${var.DomainDNSName}"
 }
